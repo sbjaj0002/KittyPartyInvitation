798fc2ee7046583e0ce76258a473e144b579c513
sample <sbjaj33@gmail.com>
2021-02-10 15:55:43 +0530

    f commit

---
+++ sample.js
@@ -0,0 +1,7 @@
+function myFunction() {
+ if(document.getElementById("checkbox").checked) {
+     document.getElementById("card").id = "card_after";
+  }
+  else
+   document.getElementById("card_after").id = "card";
+}
